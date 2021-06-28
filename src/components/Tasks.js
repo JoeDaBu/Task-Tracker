583622a936c03730b9cdfdb7d3437d5f99a246cc
JoeDaBu <joegbu@gmail.com>
2021-06-27 18:37:03 -0600

    Tasks in component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,5 +1,7 @@
-const tasks = [
-    {
+import { useState } from 'react'
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState([{
         id: 1,
         text: 'Doctors Appointment',
         day: 'Feb 5th at 2:30pm',
@@ -8,12 +10,7 @@ const tasks = [
     {
         id:2,
         text: 'Meeting at School',
-    }
-
-
-]
-
-const Tasks = () => {
+    }])
     return (
         <>
          {tasks.map((task) => 
